6a82c0ee10e253f13aaa326c42d677e4fde13618
Dave <dave.flanagan@nolwenture.com>
2021-08-24 10:46:17 +0300

    Add privacy policy

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -2,10 +2,11 @@ import React, { useState } from 'react';
 import {
   BrowserRouter as Router, Switch, Route, Link,
 } from 'react-router-dom';
+import moment from 'moment';
 
 import './App.scss';
-import Column from './Components/Column';
 
+import Column from './Components/Column';
 import Container from './Components/Container';
 import Header from './Components/Header';
 import Navigation from './Components/Navigation';
@@ -16,6 +17,7 @@ import Home from './Views/Home';
 import Photography from './Views/Photography';
 import Projects from './Views/Projects';
 import Contact from './Views/Contact';
+import Privacy from './Views/Privacy';
 
 const App: React.FC = () => (
   <Router>
@@ -31,6 +33,9 @@ const App: React.FC = () => (
             <Route exact path="/">
               <Home />
             </Route>
+            <Route path="/privacy">
+              <Privacy />
+            </Route>
             <Route path="/projects">
               <Projects />
             </Route>
@@ -44,7 +49,8 @@ const App: React.FC = () => (
         </Column>
       </Row>
       <footer className=" d-flex">
-        &copy; Copyright 2021 Dave Flanagan
+        &copy; Copyright {moment().format('YYYY')} Dave Flanagan
+        <Link className="ms-auto" to="/privacy">Privacy Policy</Link>
       </footer>
     </Container>
   </Router>
